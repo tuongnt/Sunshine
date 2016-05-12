6213d1a1a1807ccd8623084121becb5d7c8394c0
Tuong Nguyen <tuongnt.dev@gmail.com>
2016-05-13 01:00:07 +0700

    update error message

--- Sunshine/app/src/main/java/me/tuongnt/sunshine/ui/home/view/HomeFragment.java
+++ Sunshine/app/src/main/java/me/tuongnt/sunshine/ui/home/view/HomeFragment.java
@@ -1,11 +1,16 @@
 package me.tuongnt.sunshine.ui.home.view;
 
 import android.util.Log;
+import android.widget.ViewSwitcher;
 
+import butterknife.Bind;
 import me.tuongnt.sunshine.R;
 import me.tuongnt.sunshine.app.component.AppComponent;
+import me.tuongnt.sunshine.ui.SunshineError;
 import me.tuongnt.sunshine.ui.common.BaseFragment;
 import me.tuongnt.sunshine.ui.home.viewmodel.HomeViewModel;
+import me.tuongnt.sunshine.ui.view.ErrorView;
+import me.tuongnt.sunshine.utils.NetworkManager;
 import rx.android.schedulers.AndroidSchedulers;
 
 /**
@@ -13,6 +18,12 @@ import rx.android.schedulers.AndroidSchedulers;
  */
 public class HomeFragment extends BaseFragment<HomeViewModel> {
     private static final String TAG = HomeFragment.class.getSimpleName();
+
+    @Bind(R.id.content_switcher)
+    ViewSwitcher mViewSwitcher;
+
+    @Bind(R.id.error_view)
+    ErrorView mErrorView;
 
     @Override
     public void onStart() {
@@ -32,12 +43,27 @@ public class HomeFragment extends BaseFragment<HomeViewModel> {
 
     @Override
     protected void bindViewModel() {
+
+        final NetworkManager networkManager = new NetworkManager(getContext());
+        mSubscriptions.add(networkManager
+                .connection()
+                .subscribe(this::handleConnectionStateChanged));
+
         mSubscriptions.add(mViewModel.onGetWeatherSuccess()
                 .observeOn(AndroidSchedulers.mainThread())
                 .filter(result -> result != null)
                 .subscribe(weather -> {
                     Log.i(TAG, weather.toString());
                 }));
+    }
+
+    private void handleConnectionStateChanged(boolean connected) {
+        if (connected) {
+            mViewSwitcher.setDisplayedChild(0);
+        } else {
+            mErrorView.showError(SunshineError.NETWORK_ERROR);
+            mViewSwitcher.setDisplayedChild(1);
+        }
     }
 
 }
